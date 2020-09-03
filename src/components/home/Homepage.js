6dee3692d4354c03793991080c2b85e89f9ecdaa
Luke Esworthy <lukesworthy@gmail.com>
2020-09-03 11:48:29 -0500

    got product cards displaying on homepage

--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from "react";
+import HomepageProdCard from "./HomepageProdCard";
 
 const HomepageProductList = (props) => {
   const [productInfo, setProductInfo] = useState([]);
 
-  fetch("http://localhost:8000/products")
-    .then((result) => result.json())
-    .then((productInfo) => {
-      setProductInfo(productInfo);
-    });
+  const getProducts = () => {
+    return fetch("http://localhost:8000/products")
+      .then((result) => result.json())
+      .then((productInfo) => {
+        setProductInfo(productInfo);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="product-container-cards">
+      {productInfo.map((product) => (
+        <HomepageProdCard key={productInfo.id} product={product} {...props} />
+      ))}
+    </div>
+  );
 };
+
+export default HomepageProductList;
